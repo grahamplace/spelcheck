4740767fd90fda97887798978aa968839cee161b
Graham Place <grahamgplace@gmail.com>
2019-01-26 15:48:31 -0800

    Add empty homepage + route

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from suggest import suggest
-
 
 app = Flask(__name__)
 
 
-@app.route('/suggest/<word>')
+@app.route('/')
+def homepage(word):
+    render_template('index.html')
+
+
+@app.route('/suggest/<word>.json')
 def suggest_word(word):
     return jsonify(suggest(word))
