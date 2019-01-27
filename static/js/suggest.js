0bd6530c2064cd974796eb74b4255ca18f1f08ee
Graham Place <grahamgplace@gmail.com>
2019-01-27 15:05:49 -0800

    Improve dropdown selection UX

--- static/js/suggest.js
+++ static/js/suggest.js
@@ -1,6 +1,7 @@
 const submitButton = document.querySelector('#submit-button');
 const inputBox = document.querySelector('#input-box');
 const resultsLimit = 10;
+let selectedId = 0;
 
 const itemClick = function(event) {
   let word = event.target.innerHTML;
@@ -12,11 +13,11 @@ const writeSuggestion = function(suggestions) {
   const suggestionList = document.querySelector('#suggestion-list')
   suggestionList.style.visibility = 'visible';
   for (i = 0; i < Math.min(resultsLimit, suggestions.length); i++) {
-    suggestionList.innerHTML += '<button class="dropdown-item" type="button">' + suggestions[i] + '</button>';
+    suggestionList.innerHTML += '<button class="dropdown-item" id="dd-' + (i+1) + '" type="button" value="' + suggestions[i]  + '">' + suggestions[i] + '</button>';
   }
 
   var dropdownItems = document.getElementsByClassName('dropdown-item');
-  for (i = 0; i < dropdownItems.length; i++) {
+  for (i = i; i < dropdownItems.length; i++) {
     dropdownItems[i].addEventListener('click', itemClick);
   }
 }
@@ -38,8 +39,11 @@ const writeDefinitions = function(definitions) {
   }
 }
 
-const getSuggestion = function() {
+const getSuggestion = function(event) {
+  if (event.keyCode >= 37 && event.keyCode <= 40)
+    return;
   let inputWord = inputBox.value;
+  selectedId = 0;
   document.querySelector('#suggestion-list').innerHTML = '';
   $("#definitions").empty();
   $.get('/suggest/list/' + inputWord + '.json', success=writeSuggestion);
@@ -53,9 +57,28 @@ const getDefinition = function() {
 }
 
 $("#input-box").keydown(function (e){
-    if(e.keyCode == 13){
+    if(e.keyCode == 13)
         getDefinition();
+})
+
+$("#input-box").keydown(function (e){
+    let oldId = selectedId;
+    let oldText = inputBox.value;
+    if(e.keyCode == 38){ // up arrow key
+        selectedId = Math.max(0, selectedId - 1);
+    } else if (e.keyCode == 40) { // down arrow key
+        selectedId = Math.min(resultsLimit, selectedId + 1);
+    } else {
+      return;
     }
+    $('#dd-' + selectedId).addClass('active');
+
+    if (selectedId != oldId)
+      $('#dd-' + oldId).removeClass('active');
+
+    let selection = $('#dd-' + selectedId).val();
+    if (selection)
+      inputBox.value = selection;
 })
 
 inputBox.addEventListener('keyup', getSuggestion);
